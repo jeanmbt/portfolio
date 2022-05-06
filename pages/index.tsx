679cf504b8c0a70ca2b415ec869a7d679e7b3b33
Jean Michel Battirola <jeanbattirola@gmail.com>
2022-05-06 16:36:53 +0200

    first commit, initial configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, Container, Paper, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -12,7 +13,39 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>HI!</main>
+      <main className={styles.main}>
+        {/* TODO: Abstract into a component */}
+        <Container sx={{ paddingY: 2 }}>
+          <Box
+            component={Paper}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "auto",
+
+              height: "80vh",
+            }}
+            paddingY={5}
+          >
+            <Typography
+              sx={{ marginBottom: 5, textAlign: "center", fontSize: [50, 30] }}
+              variant="h5"
+              gutterBottom
+            >
+              Jean Michel Battirola
+            </Typography>
+            <Button
+              sx={{ width: [250], height: [100, 60], fontSize: 20 }}
+              variant="contained"
+              href="/people"
+            >
+              See all People
+            </Button>
+          </Box>
+        </Container>
+      </main>
     </div>
   );
 };
