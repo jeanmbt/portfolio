a922c74c67ea7b91bbdd9dbd6335e0b22febc557
Jean Michel Battirola <jeanbattirola@gmail.com>
2022-05-11 11:38:09 +0200

    feat: migrate form code into project

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,12 +1,38 @@
 import { Typography } from "@mui/material";
+import React from "react";
 import { Stacked } from "../../pages";
+import { useCurrentLocation } from "../../utility/useCurrentLocation";
+import { ContactForm } from "../contactForm";
 
 export const Contact = () => {
+  const [formData, setFormData] = React.useState({
+    company: "",
+    name: "",
+    phone: "",
+    email: "",
+  });
+  const [submitted, setSubmitted] = React.useState(false);
+  const [company, setCompany] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const { country, setCountry } = useCurrentLocation();
+
   return (
     <>
       <Stacked>
         <Typography variant="h4">Contact</Typography>
-        Form...
+        <ContactForm
+          country={country?.toString().toLowerCase()}
+          submitted={submitted}
+          setSubmitted={setSubmitted}
+          setFormData={setFormData}
+          formData={formData}
+          setCompany={setCompany}
+          setName={setName}
+          setPhone={setPhone}
+          setEmail={setEmail}
+        />
       </Stacked>
     </>
   );
