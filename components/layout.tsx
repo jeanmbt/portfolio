bba5ba76161ce7ed46f119bb71dccc298840e6cd
Jean Michel Battirola <jeanbattirola@gmail.com>
2022-05-12 21:20:19 +0200

    style: fix layout bug

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Box, Button, Toolbar, Typography } from "@mui/material";
 import Link from "next/link";
-
 import styles from "../styles/Home.module.css";
-
 import { ReactNode } from "react";
 import { theme, Theme } from "../styles/theme/theme";
 import { Stacked } from "../pages";
@@ -32,12 +30,14 @@ const Header = () => {
       <Box
         sx={{
           display: "flex",
-          width: "100%",
-          justifyContent: "space-around",
+          width: "60%",
+          justifyContent: "space-between",
           alignItems: "center",
           flexWrap: "wrap",
           margin: 0,
           color: theme.palette.text.secondary,
+          fontWeight: "bold",
+          fontSize: "0.8em",
         }}
       >
         <Link href="/" passHref>
