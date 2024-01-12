f483d48af6e3f1af4bf53a40e6750645011a6d1b
Jean Michel Battirola <66276446+jeanmbt@users.noreply.github.com>
2024-01-12 21:41:57 +0100

    [ main ]-( style ): remove unnecessary

--- components/stickyLinks.tsx
+++ components/stickyLinks.tsx
@@ -1,162 +1,116 @@
-import { Box, Button, ButtonGroup, Tooltip, IconButton, Menu, MenuItem } from "@mui/material";
-import React from "react";
+import { Box, Button, ButtonGroup, Tooltip } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import GroupsIcon from "@mui/icons-material/Groups";
-import PersonIcon from "@mui/icons-material/Person";
 import { useRouter } from "next/router";
+import PersonIcon from "@mui/icons-material/Person";
 import { theme } from "../styles/theme/theme";
-import useMediaQuery from '@mui/material/useMediaQuery';
-import MenuIcon from '@mui/icons-material/Menu';
-
+// TODO: Make list, use icons
 export const StickyLinks = () => {
   const router = useRouter();
-  const isSmallScreen = useMediaQuery('(max-width: 600px)');
-  const [menuOpen, setMenuOpen] = React.useState(false);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const buttonData = [
-    {
-      title: "Back to my profile",
-      icon: <PersonIcon />,
-      href: "/",
-      condition: router.route === "/cv",
-      sx: {
-        width: 0,
-        value: `Back to my profile`,
-        ariaLabel: `Back to my profile`,
-        fontWeight: "bold",
-        bgcolor: theme.palette.secondary.main,
-        "&:hover": { bgcolor: theme.palette.secondary.dark },
-      },
-    },
-    {
-      title: "GitHub",
-      icon: <GitHubIcon />,
-      href: "http://github.com/jeanmbt",
-      sx: {
-        width: 0,
-        value: `GitHub`,
-        ariaLabel: `GitHub`,
-      },
-    },
-    {
-      title: "Email",
-      icon: <EmailIcon />,
-      href: "mailto:jeanbattirola@gmail.com",
-      sx: {
-        width: 0,
-        value: `Send Jean Michel Battirola an Email`,
-        ariaLabel: `Send Jean Michel Battirola an Email`,
-      },
-    },
-    {
-      title: "LinkedIn",
-      icon: <LinkedInIcon />,
-      href: "http://linkedin.com/in/jeanmbt",
-      sx: {
-        width: 0,
-        value: `LinkedIn`,
-        ariaLabel: `LinkedIn`,
-      },
-    },
-    {
-      title: "CV",
-      icon: "CV",
-      href: "/cv",
-      condition: router.route !== "/cv",
-      sx: {
-        width: 0,
-        value: `CV`,
-        ariaLabel: `CV`,
-        fontWeight: "bold",
-      },
-    },
-    {
-      title: "Meet Me",
-      icon: <GroupsIcon />,
-      href: "https://calendly.com/jeanbattirola/30min",
-      sx: {
-        value: `Meet Me`,
-        ariaLabel: `Meet Me`,
-        fontWeight: "bold",
-        width: 0,
-      },
-    },
-  ];
-
-  const handleMenuClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
-    setMenuOpen(true);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-    setMenuOpen(false);
-  };
 
   return (
     <Box
       marginY={2}
       padding={1}
-      sx={{
-        position: isSmallScreen ? "fixed" : "sticky", 
-        right: isSmallScreen ? "0" : "100%", 
-        top: isSmallScreen ? "-3.5%" : "35%",
-        paddingX: 1,
-        zIndex: 1000,
-      }}
+      sx={{position: "sticky", right: "100%", top: "35%", paddingX: 1}}
     >
-      {isSmallScreen ? (
-        <IconButton onClick={handleMenuClick}>
-          <MenuIcon
+      <ButtonGroup orientation="vertical">
+        {router.route==="/cv" && (
+          <Tooltip title="Back to my profile">
+            <Button
+              variant="contained"
+              href="/"
+              sx={{
+                width: 0,
+                value: `Back to my profile`,
+                ariaLabel: `Back to my profile`,
+                fontWeight: "bold",
+                bgcolor: theme.palette.secondary.main,
+                "&:hover": {bgcolor: theme.palette.secondary.dark},
+              }}
+            >
+              <PersonIcon/>
+            </Button>
+          </Tooltip>
+        )}
+        <Tooltip title="GitHub">
+          <Button
+            target="_blank"
+            variant="contained"
             sx={{
-              color: theme.palette.secondary,
-              fontSize: "2rem",
-              backgroundColor: theme.palette.primary,
-              borderRadius: "100%"
+              width: 0,
+              value: `GitHub`,
+              ariaLabel: `GitHub`,
             }}
-          />
-        </IconButton>
-      ) : (
-        <ButtonGroup orientation="vertical">
-          {buttonData.map((button, index) => (
-            button.condition === undefined || button.condition ? (
-              <Tooltip key={index} title={button.title} placement="right">
-                <Button
-                  variant="contained"
-                  href={button.href}
-                  sx={button.sx}
-                >
-                  {button.icon}
-                </Button>
-              </Tooltip>
-            ) : null
-          ))}
-        </ButtonGroup>
-      )}
+            href="http://github.com/jeanmbt"
+          >
+            <GitHubIcon/>
+          </Button>
+        </Tooltip>
+        <Tooltip title="Email">
+          <Button
+            sx={{
+              width: 0,
+              value: `Send Jean Michel Battirola a Email`,
+              ariaLabel: `Send Jean Michel Battirola a Email`,
+            }}
+            target="_blank"
+            size="small"
+            variant="contained"
+            href="mailto:dev@jeanbattirola.com"
+          >
+            <EmailIcon/>
+          </Button>
+        </Tooltip>
+        <Tooltip title="LinkedIn">
+          <Button
+            sx={{
+              width: 0,
+              value: `LinkedIn`,
+              ariaLabel: `LinkedIn`,
+            }}
+            target="_blank"
+            variant="contained"
+            href="http://linkedin.com/in/jeanmbt"
+          >
+            <LinkedInIcon/>
+          </Button>
+        </Tooltip>
+        {router.route==="/" && (
+          <Tooltip title="Resume">
+            <Button
+              variant="contained"
+              href="/cv"
+              sx={{
+                width: 0,
+                value: `CV`,
+                ariaLabel: `CV`,
+                fontWeight: "bold",
+              }}
+            >
+              CV
+            </Button>
+          </Tooltip>
+        )}
 
-      <Menu
-        anchorEl={anchorEl}
-        open={menuOpen}
-        onClose={handleClose}
-        sx={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-      >
-        {buttonData.map((button, index) => (
-          button.condition === undefined || button.condition ? (
-            <MenuItem key={index} onClick={handleClose}>
-              <a 
-                href={button.href} 
-                style={{ textDecoration: "none", color: "inherit", padding: "0.5em" }}
-              >
-                {button.title}
-              </a>
-            </MenuItem>
-          ) : null
-        ))}
-      </Menu>
+        <Tooltip title="Meet Me">
+          <Button
+            sx={{
+              value: `Meet Me`,
+              ariaLabel: `Meet Me`,
+              fontWeight: "bold",
+              width: 0,
+            }}
+            target="_blank"
+            variant="contained"
+            href="https://calendly.com/jeanbattirola/30min"
+          >
+            <GroupsIcon/>
+          </Button>
+        </Tooltip>
+      </ButtonGroup>
     </Box>
   );
-
-};
+}
--- utility/projectsData.ts
+++ utility/projectsData.ts
@@ -11,11 +11,12 @@ export interface Project {
 }
 
 export  const projectsData: Project[] = [
+  //todo: add myway
   {
     title: "Bryan",
     image: `${bryan.src}`,
     techs: ["langchain", "open-source AI models", "python", "autogen",], 
-    description: "Conceptualized and implemented a internal AI-powered management and support tool for Brayn.io. Developed the front-end, the machine learning models, and the back-end. Allows managing the company's projects, and to provide general information about them to the developers, performing RAG (Retrieval-Augmented Generation) on the Documents Base and Agents that fetch data from Sources like Jira. It also provides multiple chats with OpenAI's GPT4 and Assistants API.",
+    description: "Conceptualized and implemented a internal AI-powered management and support tool for Brayn.io. Developed the front-end, the machine learning models back-end and the back-end. Allows managing the company's projects, and to provide general information about them to the developers, performing RAG (Retrieval-Augmented Generation) on the Documents Base and Agents that fetch data from Sources like Jira. It also provides multiple chats with OpenAI's GPT4 and Assistants API.",
   },
   {
     title: "Sicherheit-Nord",
