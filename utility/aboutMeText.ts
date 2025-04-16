d915a46900afd5b91c60f4086a7efc38f7d31e41
Jean Michel Battirola <66276446+jeanmbt@users.noreply.github.com>
2025-04-16 16:39:59 +0200

    main: fix ( core ) - improve description

--- utility/aboutMeText.ts
+++ utility/aboutMeText.ts
@@ -1,8 +1,8 @@
 export const aboutMeText = `Software Engineer with a special interest in Artificial Intelligence and a holistic approach, adept at balancing meticulous attention to detail with a overarching vision. 
 
-My expertise: high-quality, maintainable, and collaborative code using the technologies: Vue.js, Pinia, TypeScript, Python, Git, Docker, Quasar, HTML, SCSS and practices like continuous integration.
+My expertise: high-quality, maintainable, and collaborative code using the technologies: Python, Vue.js, Pinia, TypeScript, Git, Docker, Quasar, HTML, SCSS, other Backend and Frontend frameworks and tools, and practices like continuous integration.
 
-I excel at identifying issues ahead of time and offering innovative solutions that not only surprise with their effectiveness and simplicity, but also demonstrate lasting impact. 
+I excel at identifying issues ahead of time and offering innovative solutions fast, that not only surprise with their effectiveness and simplicity, but also demonstrate lasting impact. 
 
 With a background in the arts, my approach to technology is infused with innovation and originality. In team settings, my strengths are empathy and a collaborative spirit, coupled with adaptability to various team dynamics and honest communication.
 `
